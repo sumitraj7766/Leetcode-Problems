ed45d6c141564f0a4b6195a613b5f584a0d38966
sumit Kumar <sk681172112209@gmail.com>
2026-07-06 08:42:59 +0530

    Time: 11 ms (23.07%), Space: 23.4 MB (24.85%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,14 @@
-class Solution(object):
+class Solution:
     def rotate(self, nums, k):
         n = len(nums)
-        k = k % n
+        k %= n
 
-        nums[:] = nums[-k:] + nums[:-k]
-        
-        
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
