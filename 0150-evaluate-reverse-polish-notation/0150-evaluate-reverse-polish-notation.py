8d136b56133538a1ab727ffe324b6422f5208597
sumit Kumar <sk681172112209@gmail.com>
2026-07-31 14:34:57 +0530

    Time: 5 ms (82.82%), Space: 13.7 MB (11.42%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack = []
+
+
+     
+        for token in tokens:
+            if token not in "+-*/":
+                stack.append(int(token))
+
+            else:
+                b = stack.pop()
+                a = stack.pop()
+
+                if token == "+":
+                    stack.append(a+b)
+
+                elif token == "-":
+                    stack.append(a-b)
+
+                elif token == "*":
+                    stack.append(a*b)
+
+                else:
+                    stack.append(int(float(a) / b))
+
+        return stack[-1]
