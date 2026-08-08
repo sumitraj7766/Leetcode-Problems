24437232a2b4dfe100932e5b5542e5130ae16988
sumit Kumar <sk681172112209@gmail.com>
2026-08-08 09:57:28 +0530

    Time: 23 ms (6.53%), Space: 12.4 MB (14.67%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstBadVersion(self, n):
+        low = 1
+        high = n
+
+        while low < high:
+            mid = low + (high - low) // 2
+
+            if isBadVersion(mid):
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
