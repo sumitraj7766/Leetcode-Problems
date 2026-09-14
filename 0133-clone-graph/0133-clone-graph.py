091659eb39ce0e156f912e1bed3e4a317883fe7c
sumit Kumar <sk681172112209@gmail.com>
2026-09-14 11:52:13 +0530

    Time: 33 ms (70.17%), Space: 12.6 MB (69.85%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        if not node:
+            return None
+
+        clones = {node : Node(node.val)}
+        queue = deque([node])
+
+        while queue:
+            current = queue.popleft()
+
+            for neighbor in current.neighbors:
+                if neighbor not in clones:
+                    clones[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+
+                clones[current].neighbors.append(clones[neighbor])
+
+        return clones[node]
+
+
+
+
+
+        
+        
