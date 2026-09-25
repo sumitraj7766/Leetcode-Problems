c8fd1e32185cd2d42a5d455f85853f8de48006b8
sumit Kumar <sk681172112209@gmail.com>
2026-09-25 10:01:52 +0530

    Time: 3 ms (55.89%), Space: 12.4 MB (97.39%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        chars = list(s)
+        n = len(chars)
+
+        for i in range(0, n, 2 * k ):
+            left = i
+            right = min(i + k - 1, n - 1)
+
+            while left < right:
+                chars[left], chars[right] = chars[right], chars[left]
+
+                left += 1
+                right -= 1
+
+        return "".join(chars)
+        
